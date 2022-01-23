844ca143e1fe294b7cfb0c3c5ccb9adde9a125f1
bruthabryce <68303137+bruthabryce@users.noreply.github.com>
2022-01-23 17:14:52 -0500

    Test

---
+++ scroll.js
@@ -0,0 +1,19 @@
+const body = document.body,
+    scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
+    height = scrollWrap.getBoundingClientRect().height - 1,
+    speed = 0.05;
+
+let offset = 0;
+
+body.style.height = Math.floor(height) + "px";
+
+function smoothScroll() {
+    offset += (window.pageYOffset - offset) * speed;
+
+    let scroll = "translateY(-" + offset + "px) translateZ(0)";
+    scrollWrap.style.transform = scroll;
+
+    callScroll = requestAnimationFrame(smoothScroll);
+}
+
+smoothScroll();
